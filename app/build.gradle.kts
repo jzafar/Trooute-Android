d5d367377d481aa69c924c2a14a2bc19f0ed84d9
Muhammad Jahangir Zafar <muhammad.zafar@nexusgroup.com>
2024-03-25 08:59:34 +0100

    next version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.travel.trooute"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
